8b409208d3e5bdeff689f89bfcfe276b6bfb8bae
jacobaroberts1993 <148710064+jacobaroberts1993@users.noreply.github.com>
2023-11-27 17:01:27 -0600

    Project Slides / Buttons

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ const aboutSlide = document.querySelector(".about");
 const emailLink = document.querySelector(".email-address-link");
 const frontEnd = document.querySelector("#front-end-text");
 const email = document.querySelector("#email-text");
+const projectButtonOne = document.querySelector(".first-project");
+const projectButtonTwo = document.querySelector(".second-project");
+const projectButtonThree = document.querySelector(".third-project");
+const projectOne = document.querySelector(".card-one");
+const projectTwo = document.querySelector(".card-two");
+const projectThree = document.querySelector(".card-three");
 
 function showSlide(slideToShow) {
   heroSlide.classList.add("hidden");
@@ -32,4 +38,22 @@ projectsLink.addEventListener("click", function () {
 emailLink.addEventListener("click", function () {
   frontEnd.classList.toggle("hidden");
   email.classList.toggle("hidden");
+});
+
+function showProject(projectToShow) {
+  projectOne.classList.add("hidden");
+  projectTwo.classList.add("hidden");
+  projectThree.classList.add("hidden");
+  projectToShow.classList.remove("hidden");
+}
+
+projectButtonOne.addEventListener("click", function () {
+  showProject(projectOne);
+});
+
+projectButtonTwo.addEventListener("click", function () {
+  showProject(projectTwo);
+});
+projectButtonThree.addEventListener("click", function () {
+  showProject(projectThree);
 });
